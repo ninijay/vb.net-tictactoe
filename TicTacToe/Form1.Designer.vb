c0130a63fa5b83b2bd53db5a8b04d3d4a749069c
Zanidd <ninijay93@gmail.com>
2016-05-19 09:33:45 +0200

    Initial Files from 2012

---
+++ TicTacToe/Form1.Designer.vb
@@ -0,0 +1,209 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmTicTacToe
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.sqr1_3 = New System.Windows.Forms.PictureBox()
+        Me.sqr2_3 = New System.Windows.Forms.PictureBox()
+        Me.sqr3_3 = New System.Windows.Forms.PictureBox()
+        Me.sqr1_2 = New System.Windows.Forms.PictureBox()
+        Me.sqr2_2 = New System.Windows.Forms.PictureBox()
+        Me.sqr3_2 = New System.Windows.Forms.PictureBox()
+        Me.sqr1_1 = New System.Windows.Forms.PictureBox()
+        Me.sqr2_1 = New System.Windows.Forms.PictureBox()
+        Me.sqr3_1 = New System.Windows.Forms.PictureBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.Panel4 = New System.Windows.Forms.Panel()
+        CType(Me.sqr1_3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.sqr2_3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.sqr3_3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.sqr1_2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.sqr2_2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.sqr3_2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.sqr1_1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.sqr2_1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.sqr3_1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'sqr1_3
+        '
+        Me.sqr1_3.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.sqr1_3.Location = New System.Drawing.Point(0, 0)
+        Me.sqr1_3.Name = "sqr1_3"
+        Me.sqr1_3.Size = New System.Drawing.Size(150, 150)
+        Me.sqr1_3.TabIndex = 0
+        Me.sqr1_3.TabStop = False
+        '
+        'sqr2_3
+        '
+        Me.sqr2_3.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.sqr2_3.Location = New System.Drawing.Point(150, 0)
+        Me.sqr2_3.Name = "sqr2_3"
+        Me.sqr2_3.Size = New System.Drawing.Size(150, 150)
+        Me.sqr2_3.TabIndex = 1
+        Me.sqr2_3.TabStop = False
+        '
+        'sqr3_3
+        '
+        Me.sqr3_3.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.sqr3_3.Location = New System.Drawing.Point(300, 0)
+        Me.sqr3_3.Name = "sqr3_3"
+        Me.sqr3_3.Size = New System.Drawing.Size(150, 150)
+        Me.sqr3_3.TabIndex = 2
+        Me.sqr3_3.TabStop = False
+        '
+        'sqr1_2
+        '
+        Me.sqr1_2.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.sqr1_2.Location = New System.Drawing.Point(0, 150)
+        Me.sqr1_2.Name = "sqr1_2"
+        Me.sqr1_2.Size = New System.Drawing.Size(150, 150)
+        Me.sqr1_2.TabIndex = 3
+        Me.sqr1_2.TabStop = False
+        '
+        'sqr2_2
+        '
+        Me.sqr2_2.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.sqr2_2.Location = New System.Drawing.Point(150, 150)
+        Me.sqr2_2.Name = "sqr2_2"
+        Me.sqr2_2.Size = New System.Drawing.Size(150, 150)
+        Me.sqr2_2.TabIndex = 4
+        Me.sqr2_2.TabStop = False
+        '
+        'sqr3_2
+        '
+        Me.sqr3_2.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.sqr3_2.Location = New System.Drawing.Point(300, 150)
+        Me.sqr3_2.Name = "sqr3_2"
+        Me.sqr3_2.Size = New System.Drawing.Size(150, 150)
+        Me.sqr3_2.TabIndex = 5
+        Me.sqr3_2.TabStop = False
+        '
+        'sqr1_1
+        '
+        Me.sqr1_1.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.sqr1_1.Location = New System.Drawing.Point(0, 300)
+        Me.sqr1_1.Name = "sqr1_1"
+        Me.sqr1_1.Size = New System.Drawing.Size(150, 150)
+        Me.sqr1_1.TabIndex = 6
+        Me.sqr1_1.TabStop = False
+        '
+        'sqr2_1
+        '
+        Me.sqr2_1.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.sqr2_1.Location = New System.Drawing.Point(150, 300)
+        Me.sqr2_1.Name = "sqr2_1"
+        Me.sqr2_1.Size = New System.Drawing.Size(150, 150)
+        Me.sqr2_1.TabIndex = 7
+        Me.sqr2_1.TabStop = False
+        '
+        'sqr3_1
+        '
+        Me.sqr3_1.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.sqr3_1.Location = New System.Drawing.Point(300, 300)
+        Me.sqr3_1.Name = "sqr3_1"
+        Me.sqr3_1.Size = New System.Drawing.Size(150, 150)
+        Me.sqr3_1.TabIndex = 8
+        Me.sqr3_1.TabStop = False
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.SystemColors.Control
+        Me.Panel1.Location = New System.Drawing.Point(148, 11)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(2, 417)
+        Me.Panel1.TabIndex = 9
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.SystemColors.Control
+        Me.Panel2.Location = New System.Drawing.Point(300, 11)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(2, 417)
+        Me.Panel2.TabIndex = 10
+        '
+        'Panel3
+        '
+        Me.Panel3.BackColor = System.Drawing.SystemColors.Control
+        Me.Panel3.Location = New System.Drawing.Point(32, 148)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(394, 2)
+        Me.Panel3.TabIndex = 10
+        '
+        'Panel4
+        '
+        Me.Panel4.BackColor = System.Drawing.SystemColors.Control
+        Me.Panel4.Location = New System.Drawing.Point(32, 300)
+        Me.Panel4.Name = "Panel4"
+        Me.Panel4.Size = New System.Drawing.Size(394, 2)
+        Me.Panel4.TabIndex = 11
+        '
+        'frmTicTacToe
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.ClientSize = New System.Drawing.Size(450, 451)
+        Me.Controls.Add(Me.Panel4)
+        Me.Controls.Add(Me.Panel3)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.sqr3_1)
+        Me.Controls.Add(Me.sqr2_1)
+        Me.Controls.Add(Me.sqr1_1)
+        Me.Controls.Add(Me.sqr3_2)
+        Me.Controls.Add(Me.sqr2_2)
+        Me.Controls.Add(Me.sqr1_2)
+        Me.Controls.Add(Me.sqr3_3)
+        Me.Controls.Add(Me.sqr2_3)
+        Me.Controls.Add(Me.sqr1_3)
+        Me.Name = "frmTicTacToe"
+        Me.Text = "Form1"
+        CType(Me.sqr1_3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.sqr2_3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.sqr3_3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.sqr1_2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.sqr2_2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.sqr3_2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.sqr1_1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.sqr2_1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.sqr3_1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents sqr1_3 As System.Windows.Forms.PictureBox
+    Friend WithEvents sqr2_3 As System.Windows.Forms.PictureBox
+    Friend WithEvents sqr3_3 As System.Windows.Forms.PictureBox
+    Friend WithEvents sqr1_2 As System.Windows.Forms.PictureBox
+    Friend WithEvents sqr2_2 As System.Windows.Forms.PictureBox
+    Friend WithEvents sqr3_2 As System.Windows.Forms.PictureBox
+    Friend WithEvents sqr1_1 As System.Windows.Forms.PictureBox
+    Friend WithEvents sqr2_1 As System.Windows.Forms.PictureBox
+    Friend WithEvents sqr3_1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents Panel2 As System.Windows.Forms.Panel
+    Friend WithEvents Panel3 As System.Windows.Forms.Panel
+    Friend WithEvents Panel4 As System.Windows.Forms.Panel
+
+End Class
---
+++ TicTacToe/Form1.vb
@@ -0,0 +1,377 @@
+﻿
+
+
+
+Public Class frmTicTacToe
+
+#Region " Global declarations"
+
+
+
+    Inherits System.Windows.Forms.Form
+
+    Dim PlayerTurn As Integer
+
+    Dim ClickedSquare(3, 3) As Boolean
+
+    Dim ClickedX(3, 3) As Boolean
+    Dim ClickedO(3, 3) As Boolean
+    Dim sqr(3, 3) As PictureBox
+
+
+#End Region
+
+
+#Region ".Click Events"
+
+
+
+    Private Sub sqr1_1_Click(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+
+        If ClickedSquare(1, 1) = False Then
+
+            If PlayerTurn Mod 2 = 0 Then
+                sqr1_1.Image = Image.FromFile("x.jpg")
+                PlayerTurn += 1
+                ClickedX(1, 1) = True
+            ElseIf PlayerTurn Mod 2 = 1 Then
+                sqr1_1.Image = Image.FromFile("o.jpg")
+                PlayerTurn += 1
+
+                ClickedO(1, 1) = True
+            End If
+
+            ClickedSquare(1, 1) = True
+
+        End If
+
+
+        CheckForWinnerO()
+        CheckForWinnerX()
+        If ClickedSquare(3, 1) = False Then
+
+            If PlayerTurn Mod 2 = 0 Then
+                sqr2_1.Image = Image.FromFile("x.jpg")
+                PlayerTurn += 1
+                ClickedX(2, 1) = True
+            ElseIf PlayerTurn Mod 2 = 1 Then
+                sqr2_1.Image = Image.FromFile("o.jpg")
+                PlayerTurn += 1
+                ClickedO(2, 1) = True
+            End If
+
+            ClickedSquare(2, 1) = True
+
+
+        End If
+
+
+
+
+
+        CheckForWinnerO()
+        CheckForWinnerX()
+
+
+
+  
+        If ClickedSquare(3, 1) = False Then
+
+            If PlayerTurn Mod 2 = 0 Then
+                sqr3_1.Image = Image.FromFile("x.jpg")
+                PlayerTurn += 1
+                ClickedX(3, 1) = True
+            ElseIf PlayerTurn Mod 2 = 1 Then
+                sqr3_1.Image = Image.FromFile("o.jpg")
+                PlayerTurn += 1
+                ClickedO(3, 1) = True
+            End If
+            ClickedSquare(3, 1) = True
+
+        End If
+
+      
+
+        CheckForWinnerO()
+        CheckForWinnerX()
+
+
+        If ClickedSquare(1, 2) = False Then
+            If PlayerTurn Mod 2 = 0 Then
+                sqr1_2.Image = Image.FromFile("x.jpg")
+                PlayerTurn += 1
+                ClickedX(1, 2) = True
+            ElseIf PlayerTurn Mod 2 = 1 Then
+                sqr1_2.Image = Image.FromFile("o.jpg")
+                PlayerTurn += 1
+                ClickedO(1, 2) = True
+            End If
+
+            ClickedSquare(1, 2) = True
+
+        End If
+
+      
+
+
+        CheckForWinnerO()
+        CheckForWinnerX()
+
+
+
+ 
+        If ClickedSquare(2, 2) = False Then
+            If PlayerTurn Mod 2 = 0 Then
+                sqr2_2.Image = Image.FromFile("x.jpg")
+                PlayerTurn += 1
+                ClickedX(2, 2) = True
+            ElseIf PlayerTurn Mod 2 = 1 Then
+                sqr2_2.Image = Image.FromFile("o.jpg")
+                PlayerTurn += 1
+                ClickedO(2, 2) = True
+            End If
+
+            ClickedSquare(2, 2) = True
+
+        End If
+
+        
+        CheckForWinnerO()
+        CheckForWinnerX()
+
+
+        If ClickedSquare(3, 2) = False Then
+
+            If PlayerTurn Mod 2 = 0 Then
+                sqr3_2.Image = Image.FromFile("x.jpg")
+                PlayerTurn += 1
+                ClickedX(3, 2) = True
+            ElseIf PlayerTurn Mod 2 = 1 Then
+                sqr3_2.Image = Image.FromFile("o.jpg")
+                PlayerTurn += 1
+                ClickedO(3, 2) = True
+
+            End If
+            ClickedSquare(3, 2) = True
+
+
+        End If
+
+   
+
+        CheckForWinnerO()
+        CheckForWinnerX()
+
+
+        If ClickedSquare(1, 3) = False Then
+
+
+            If PlayerTurn Mod 2 = 0 Then
+                sqr1_3.Image = Image.FromFile("x.jpg")
+                PlayerTurn += 1
+                ClickedX(1, 3) = True
+            ElseIf PlayerTurn Mod 2 = 1 Then
+                sqr1_3.Image = Image.FromFile("o.jpg")
+                PlayerTurn += 1
+                ClickedO(1, 3) = True
+            End If
+            ClickedSquare(1, 3) = True
+
+        End If
+
+
+
+
+        CheckForWinnerO()
+        CheckForWinnerX()
+
+
+
+
+        If ClickedSquare(2, 3) = False Then
+
+            If PlayerTurn Mod 2 = 0 Then
+                sqr2_3.Image = Image.FromFile("x.jpg")
+                PlayerTurn += 1
+                ClickedX(2, 3) = True
+            ElseIf PlayerTurn Mod 2 = 1 Then
+                sqr2_3.Image = Image.FromFile("o.jpg")
+                PlayerTurn += 1
+                ClickedO(2, 3) = True
+            End If
+            ClickedSquare(2, 3) = True
+
+        End If
+
+
+
+        CheckForWinnerO()
+        CheckForWinnerX()
+
+
+
+        If ClickedSquare(3, 3) = False Then
+
+            If PlayerTurn Mod 2 = 0 Then
+                sqr3_3.Image = Image.FromFile("x.jpg")
+                PlayerTurn += 1
+                ClickedX(3, 3) = True
+            ElseIf PlayerTurn Mod 2 = 1 Then
+                sqr3_3.Image = Image.FromFile("o.jpg")
+                PlayerTurn += 1
+
+                ClickedO(3, 3) = True
+            End If
+            ClickedSquare(3, 3) = True
+
+        End If
+
+       
+
+
+        CheckForWinnerX()
+        CheckForWinnerO()
+
+
+    End Sub
+
+#End Region
+
+
+#Region "User Created Subs"
+
+
+    Private Sub Sound()
+
+
+        My.Computer.Audio.Play("C:\Windows\Media\tada.wav")
+
+
+    End Sub
+
+
+
+
+
+    Private Sub CheckForWinnerX()
+
+        If ClickedX(1, 1) And ClickedX(1, 2) And ClickedX(1, 3) Then
+            Sound()
+            MessageBox.Show("Player X won!")
+        ElseIf ClickedX(2, 1) And ClickedX(2, 2) And ClickedX(2, 3) Then
+            Sound()
+            MessageBox.Show("Player X won!")
+
+        ElseIf ClickedX(3, 1) And ClickedX(3, 2) And ClickedX(3, 3) Then
+            Sound()
+            MessageBox.Show("Player X won!")
+
+        End If
+
+
+        If ClickedX(1, 1) And ClickedX(2, 1) And ClickedX(3, 1) Then
+            Sound()
+            MessageBox.Show("Player X won!")
+
+        ElseIf ClickedX(1, 2) And ClickedX(2, 2) And ClickedX(3, 2) Then
+            Sound()
+            MessageBox.Show("Player X won!")
+
+        ElseIf ClickedX(1, 3) And ClickedX(2, 3) And ClickedX(3, 3) Then
+            Sound()
+
+            MessageBox.Show("Player X won!")
+
+        End If
+
+
+        If ClickedX(1, 3) And ClickedX(2, 2) And ClickedX(3, 1) Then
+            Sound()
+
+            MessageBox.Show("Player X won!")
+
+        ElseIf ClickedX(1, 1) And ClickedX(2, 2) And ClickedX(3, 3) Then
+            Sound()
+
+            MessageBox.Show("Player X won!")
+
+        End If
+
+
+    End Sub
+
+
+    Private Sub CheckForWinnerO()
+
+
+
+
+        If ClickedO(1, 1) And ClickedO(1, 2) And ClickedO(1, 3) Then
+            Sound()
+
+            MessageBox.Show("Player O won!")
+
+
+        ElseIf ClickedO(2, 1) And ClickedO(2, 2) And ClickedO(2, 3) Then
+            Sound()
+
+            MessageBox.Show("Player O won!")
+        ElseIf ClickedO(3, 1) And ClickedO(3, 2) And ClickedO(3, 3) Then
+            Sound()
+            MessageBox.Show("Player O won!")
+
+        End If
+
+
+
+
+        If ClickedO(1, 1) And ClickedO(2, 1) And ClickedO(3, 1) Then
+            Sound()
+
+            MessageBox.Show("Player O won!")
+
+
+        ElseIf ClickedO(1, 2) And ClickedO(2, 2) And ClickedO(3, 2) Then
+            Sound()
+
+            MessageBox.Show("Player O won!")
+
+
+        ElseIf ClickedO(1, 3) And ClickedO(2, 3) And ClickedO(3, 3) Then
+            Sound()
+
+            MessageBox.Show("Player O won!")
+
+
+        End If
+
+
+
+        If ClickedO(1, 3) And ClickedO(2, 2) And ClickedO(3, 1) Then
+            Sound()
+
+            MessageBox.Show("Player O won!")
+
+
+        ElseIf ClickedO(1, 1) And ClickedO(2, 2) And ClickedO(3, 3) Then
+            Sound()
+
+            MessageBox.Show("Player O won!")
+
+
+        End If
+
+
+
+
+
+
+
+    End Sub
+
+
+#End Region
+
+
+
+End Class
